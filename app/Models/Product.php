29f0fba9180b415fd412844b677f6b15cfe4e7bf
danialmahdian <danialmahdian1376@gmail.com>
2023-03-07 11:44:06 +0330

    searchable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\ProductAttributeValues;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Product extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = [
         'title',
